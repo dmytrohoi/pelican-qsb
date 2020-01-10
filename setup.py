22d26f848a6bd81fd64220d1d91ae25a6af8a26e
Hoi Dmytro <dmytro.hoi@gmail.com>
2020-01-10 17:49:50 +0200

    Update to improve the use of PyPI
    Changes:
    * Add classifiers
    * Add __init__.py with package descriptions
    * Update author and email field import in setup.py

--- setup.py
+++ setup.py
@@ -11,8 +11,8 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     url="https://github.com/dmytrohoi/pelican-qsb",
     license="MIT License",
-    author="dmytrohoi",
-    author_email="dmytro.hoi@gmail.com",
+    author=qsb.__author__,
+    author_email=qsb.__email__,
     description="Simple helper for automation Pelican-based site building",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -23,5 +23,9 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Framework :: Pelican",
+        "Framework :: Pelican :: Plugins",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools"
     ],
 )
